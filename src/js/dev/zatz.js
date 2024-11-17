b02352581e9ab6dfe8182b19485f775c8c56844e
zatzoid <zatzoid@gmail.com>
2024-11-17 13:48:27 +0300

    done

--- src/js/dev/zatz.js
+++ src/js/dev/zatz.js
@@ -1,5 +1,6 @@
 import $ from "jquery";
 import Swiper from 'swiper';
+import { Grid } from "swiper/modules";
 
 
 
@@ -21,19 +22,71 @@ function initSwipers() {
     }
 
     const howOrder = document.querySelector('.how-order__c-swiper.swiper')
-    if(howOrder){
-        new Swiper(howOrder,{
+    if (howOrder) {
+        new Swiper(howOrder, {
             slidesPerView: 1.2,
             spaceBetween: 10,
             centeredSlides: true,
-            breakpoints:{
-                768:{
+            breakpoints: {
+                768: {
                     centeredSlides: false,
                     slidesPerView: 2,
                     simulateTouch: false
                 }
             }
         })
+    }
+    const nonameShitenSlider = document.querySelector('.adv-3-blocks__c-swiper.swiper')
+    if (nonameShitenSlider) {
+        new Swiper(nonameShitenSlider, {
+            spaceBetween: 15,
+            centeredSlides: true,
+            slidesPerView: 1.2,
+            breakpoints: {
+                768: {
+                    spaceBetween: 30,
+                    slidesPerView: 3,
+                    centeredSlides: false
+
+                }
+            }
+        })
+    }
+    const startStags = document.querySelector(".start-stages")
+    if (startStags) {
+        new Swiper(startStags.querySelector('.swiper'), {
+            modules: [Grid],
+
+            slidesPerView: 1.2,
+            slidesPerGroup: 1,
+            grid: {
+                rows: 1
+            },
+            spaceBetween: 10,
+            centeredSlides: true,
+            breakpoints: {
+                768: {
+                    spaceBetween: 30,
+                    slidesPerView: 3,
+                    slidesPerGroup: 2,
+                    centeredSlides: false,
+                    grid: {
+                        fill: 'row',
+                        rows: 2
+                    },
+                }
+            }
+        })
+    }
+
+    const openStages = document.querySelector('.open-stages')
+    if (window.innerWidth < 768 && openStages) {
+        new Swiper(openStages.querySelector('.swiper'), {
+            slidesPerView: 1.2,
+            centeredSlides: true,
+            spaceBetween: 15
+        })
+
     }
 
 }
